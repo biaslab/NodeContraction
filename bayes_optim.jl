2025c7e21c93cbc1acd30fcbdcf7661075f8ec77
Wouter Nuijten <wouternuijten@gmail.com>
2024-08-20 16:16:59 +0200

    Update experiments

--- bayes_optim.jl
+++ bayes_optim.jl
@@ -2,8 +2,11 @@ using RxInfer
 using Flux, MLDatasets
 using Flux: train!, onehotbatch
 using ExponentialFamilyProjection
+using ExponentialFamily
+using ExponentialFamilyProjection.Manopt
 using ProgressMeter
 using DataStructures
+using RecursiveArrayTools
 
 struct NNFused{E,T} <: DiscreteMultivariateDistribution
     ε::E
@@ -15,7 +18,6 @@ function train_nn(model, ε, n_iter, train_data)
     f = (accuracy) -> -accuracy
     pl = Flux.plateau(f, 5; min_dist=0.01)
     es = Flux.early_stopping(f, 4)
-
     @showprogress for j in 1:n_iter
         for (i, datapoint) in enumerate(train_data)
             input, labels = datapoint
@@ -54,16 +56,13 @@ function BayesBase.logpdf(fused_neural_net::NNFused, y::AbstractMatrix{<:Real})
     ps = trained_nn(fused_neural_net.X)
 
     sumlpdf = mean(zip(eachcol(y), eachcol(ps))) do (sy, p)
-        return logpdf(Categorical(p[1:10]), argmax(sy))
-    end
-    if isinf(sumlpdf)
-        return -1000.0
+        return clamp(logpdf(Categorical(p[1:10]), argmax(sy)), -1.0f3, 1.0f3,)
     end
 
     return sumlpdf
 end;
 
-slice_size = 3000
+slice_size = 1500
 # Load training data (images, labels)
 x_train, y_train = MNIST(split=:train)[:];
 x_test, y_test = MNIST(split=:test)[:];
@@ -72,37 +71,59 @@ x_test = Flux.flatten(x_test)
 x_cutted = x_train[:, :, 1:slice_size];
 y_cutted = y_train[1:slice_size];
 
-dist = NNFused(3.0f-2, Flux.flatten(x_cutted))
+dist = NNFused(1.0f-3, Flux.flatten(x_cutted))
 logpdf(dist, Flux.onehotbatch(y_cutted, 0:9),)
 
 @node NNFused Stochastic [y, ε, X];
 
 @node Exponential Stochastic [out, rate]
-
-@model function bayes_optim(y, X)
-    ε ~ Exponential(0.00001)
+@model function bayes_optim(y, X, dist)
+    if dist == Exponential
+        ε ~ Exponential(0.00001)
+    elseif dist == Gamma
+        ε ~ Gamma(1.0, 1 / 300.0)
+    elseif dist == InverseGamma
+        ε ~ InverseGamma(1.0, 1 / 300.0)
+    end
     y ~ NNFused(ε, X)
-end;
-
-@constraints function nn_constraints()
-    parameters = ProjectionParameters(
-        strategy=ExponentialFamilyProjection.ControlVariateStrategy(nsamples=10),
-        niterations=10
-    )
-    q(ε)::ProjectedTo(Exponential; parameters=parameters)
-end;
-
-function ExponentialFamilyProjection.getinitialpoint(::ExponentialFamilyProjection.ControlVariateStrategy, M::ExponentialFamilyProjection.AbstractManifold, parameters::ProjectionParameters)
-    return [-100.0]
 end
+global result
+data = Dict()
+resulting_lrs = Dict()
+for dist in [Exponential, Gamma, InverseGamma]
 
-result = infer(
-    model=bayes_optim(X=Flux.flatten(x_cutted)),
-    data=(y=Flux.onehotbatch(y_cutted, 0:9),),
-    constraints=nn_constraints(),
-    showprogress=false,
-    options=(
-        # Read `https://reactivebayes.github.io/RxInfer.jl/stable/manuals/inference/undefinedrules/`
-        rulefallback=NodeFunctionRuleFallback(),
+    buf = IOBuffer()
+    debug = [DebugCost(io=buf), DebugDivider("\n"; io=buf)]
+
+    @constraints function nn_constraints()
+        parameters = ProjectionParameters(
+            strategy=ExponentialFamilyProjection.ControlVariateStrategy(nsamples=3),
+            niterations=3,
+        )
+        q(ε)::ProjectedTo(dist; parameters=parameters, extra=(debug=debug,))
+    end
+
+    function ExponentialFamilyProjection.getinitialpoint(::ExponentialFamilyProjection.ControlVariateStrategy, M::ExponentialFamilyProjection.AbstractManifold, parameters::ProjectionParameters)
+        if dist == Exponential
+            return [-300.0]
+        elseif dist == Gamma
+            return ArrayPartition([0.0], [-300.0])
+        elseif dist == InverseGamma
+            return ArrayPartition([-2.0], [-(1 / 300)])
+        end
+    end
+
+    result = infer(
+        model=bayes_optim(X=Flux.flatten(x_cutted), dist=dist),
+        data=(y=Flux.onehotbatch(y_cutted, 0:9),),
+        constraints=nn_constraints(),
+        showprogress=false,
+        options=(
+            # Read `https://reactivebayes.github.io/RxInfer.jl/stable/manuals/inference/undefinedrules/`
+            rulefallback=NodeFunctionRuleFallback(),
+        )
     )
-);
+    s = String(take!(buf))
+    data[dist] = parse.(Float64, getindex.(split.(split(s, "\n"), " ")[1:(end-1)], 2))
+    resulting_lrs[dist] = result.posteriors[:ε]
+end
